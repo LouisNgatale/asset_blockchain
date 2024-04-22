eb21106d819a7b68de3fa8e616a7d8d25118df38
Louis Ngatale <louisngatale@gmail.com>
2024-04-22 18:40:07 +0300

    bootstrap server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import connectBlockchain from './blockchain-connection';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from TypeScript Node.js server!');
-});
+const bootstrapServer = async () => {
+  await connectBlockchain().catch((error) => {
+    console.error('******** FAILED to run the application:', error);
+    process.exitCode = 1;
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  app.get('/', (req, res) => {
+    res.send('Hello from TypeScript Node.js server!');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+void bootstrapServer();
