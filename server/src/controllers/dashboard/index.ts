39ed4580a9c0c5f431183f04aed152a9ca6badaa
Louis Ngatale <louisngatale@gmail.com>
2024-06-06 04:01:42 +0300

    fetch citizen assets

--- server/src/controllers/dashboard/index.ts
+++ server/src/controllers/dashboard/index.ts
@@ -14,15 +14,72 @@ export default class Dashboard {
         },
       });
 
-      const allCitizens = citizens.map((citizen) => {
+      const promises = citizens.map(async (citizen) => {
         const { otp, ...rest } = citizen;
 
-        return rest;
+        // Fetch citizen assets
+        const assets = await prisma.asset.findMany({
+          where: {
+            owner: {
+              is: {
+                NIDA: citizen.NIDA,
+              },
+            },
+          },
+        });
+
+        return { ...rest, assets };
       });
+
+      const allCitizens = await Promise.all(promises);
 
       return res.json({
         success: true,
         data: allCitizens,
+      });
+    } catch (e) {
+      const error = e as Error;
+      return res.json({
+        success: false,
+        message: error.message,
+        code: ResponseCode.INTERNAL_SERVER_ERROR,
+      });
+    }
+  }
+
+  static async getStats(req: Request, res: Response) {
+    try {
+      /**
+       * - All citizens
+       * - All assets
+       * - Active deals
+       * - All admins
+       * */
+      const citizens = await prisma.user.count({
+        where: {
+          role: UserRole.CITIZEN,
+        },
+      });
+      const admins = await prisma.user.count({
+        where: {
+          role: UserRole.ADMIN,
+        },
+      });
+      const listedAssets = await prisma.asset.count({
+        where: {
+          isListed: true,
+        },
+      });
+      const allAssets = await prisma.asset.count();
+
+      return res.json({
+        success: true,
+        data: {
+          allAssets,
+          citizens,
+          admins,
+          listedAssets,
+        },
       });
     } catch (e) {
       const error = e as Error;
--- server/src/controllers/dashboard/router.ts
+++ server/src/controllers/dashboard/router.ts
@@ -4,5 +4,6 @@ import DashboardController from "./index";
 const router = Router();
 
 router.get("/citizens", DashboardController.getCitizens);
+router.get("/stats", DashboardController.getStats);
 
 export default router;
